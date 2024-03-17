0c67eaaa63de40f6ff642021b28d27f061798d7f
Nigel Charman <nigel.charman.nz@gmail.com>
2024-03-17 19:14:22 +1300

    Try with xhr

--- src/App.vue
+++ src/App.vue
@@ -23,30 +23,43 @@ export default {
   },
   methods: {
     async callAPI() {
-      try {
-        const url = new URL('https://inat-updater-test.azurewebsites.net/update');
-        url.searchParams.set('state', state);
-        url.searchParams.set('authCode', code);
-        console.log(url);
-        const response = await fetch(url, {
-          mode: "no-cors",  // TODO add Access-Control-Allow-Origin to destination
-          credentials: "omit",
-          headers: {}
-        });
-        
-        if (!response.ok) {
-          console.log(response);
-          console.log(response.status);
-          console.log(response.ok);
-          throw new Error('Network response was not ok');
+
+      const xhr = new XMLHttpRequest();
+ 
+      xhr.open('GET', 'https://inat-updater-test.azurewebsites.net/update?state=12344&code2=X');
+      
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+          console.log(xhr.responseText);
         }
-        // const data = await response.json();
-        // console.log(data);
-        // Handle the API response data here
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        // Handle error if any
       }
+      
+      xhr.send(); 
+
+      // try {
+      //   const url = new URL('https://inat-updater-test.azurewebsites.net/update');
+      //   url.searchParams.set('state', state);
+      //   url.searchParams.set('code1', code);
+      //   console.log(url);
+      //   const response = await fetch(url, {
+      //     mode: "no-cors",  // TODO add Access-Control-Allow-Origin to destination
+      //     credentials: "omit",
+      //     headers: {}
+      //   });
+        
+      //   if (!response.ok) {
+      //     console.log(response);
+      //     console.log(response.status);
+      //     console.log(response.ok);
+      //     throw new Error('Network response was not ok');
+      //   }
+      //   // const data = await response.json();
+      //   // console.log(data);
+      //   // Handle the API response data here
+      // } catch (error) {
+      //   console.error('Error fetching data:', error);
+      //   // Handle error if any
+      // }
     }
   }
 };
