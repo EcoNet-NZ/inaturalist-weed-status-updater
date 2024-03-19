0ec36b799db7bc663ed389101c8fee78d907bc30
Nigel Charman <nigel.charman.nz@gmail.com>
2024-03-19 16:47:40 +1300

    Use custom app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Hello {{ observationId }}</div>
+    <div>
+      <button @click="callAPI">Call API</button>
+    </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+const params = new URLSearchParams(window.location.search);
+// const observationId = ref('');
+// observationId.value = params.get('state'); 
+const observationId = params.get('state'); 
+console.log('Observation id is "' + observationId.value + '"');
+const code = params.get('code');
+console.log('Auth code is "' + code + '"');
+
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    async callAPI() {
+
+      try {
+        const url = new URL('/api/update', window.location.href)
+        url.searchParams.set('state', observationId)
+        url.searchParams.set('auth-code', code)
+        console.log(url)
+        const response = await fetch(url, {
+            method: "POST"
+          }
+        )
+        
+        if (!response.ok) {
+          console.log(response)
+          console.log(response.status)
+          console.log(response.ok)
+          throw new Error('Network response was not ok')
+        }
+        const text = await response.text()
+        console.log(text)
+        // const data = await response.json()
+        // console.log(data)
+        // Handle the API response data here
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        // Handle error if any
+      }
+    }
   }
 }
 </script>
