59db336adc926b8ad741e5cdc3d4c619be1649be
Nigel Charman <nigel.charman.nz@gmail.com>
2024-03-19 16:28:06 +1300

    Updated to vue3

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,26 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <div>Hello {{ observationId }}</div>
-    <div>
-      <button @click="callAPI">Call API</button>
-    </div>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import { ref } from 'vue'
-
-const params = new URLSearchParams(window.location.search);
-// const observationId = ref('');
-// observationId.value = params.get('state'); 
-const observationId = params.get('state'); 
-console.log('Observation id is "' + observationId.value + '"');
-const code = params.get('code');
-console.log('Auth code is "' + code + '"');
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  },
-  methods: {
-    async callAPI() {
-
-      try {
-        const url = new URL('/api/update', window.location.href);
-        url.searchParams.set('state', observationId);
-        url.searchParams.set('auth-code', code);
-        console.log(url);
-        const response = await fetch(url, {
-            method: "POST"
-          }
-        );
-        
-        if (!response.ok) {
-          console.log(response);
-          console.log(response.status);
-          console.log(response.ok);
-          throw new Error('Network response was not ok');
-        }
-        const text = await response.text();
-        console.log(text);
-        // const data = await response.json();
-        // console.log(data);
-        // Handle the API response data here
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        // Handle error if any
-      }
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
-};
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
