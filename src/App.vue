7b8d3349f04fa9d70f104ecd8b1e95b11e83b655
Nigel Charman <nigel.charman.nz@gmail.com>
2024-03-19 21:57:01 +1300

    Try getting message out

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,57 @@
 </script>
 
 <template>
-  <div>Updating iNaturalist observation <a :href="iNaturalistUrl">{{ observationId }}</a></div>
+  <p>Updating iNaturalist observation <a :href="iNaturalistUrl">{{ observationId }}</a></p>
   <div>
-    <button @click="callAPI">Call API</button>
+    <button @click="updateObservation">Update observation</button>
   </div>
-  <div>{{ message }}</div>
+  <p v-if="message">{{ message }}</p>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
   methods: {
-    async callAPI() {
+  //   fetchData() {
+  //     fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', {
+  //       method: "GET",
+  //       headers: {
+  //         "X-RapidAPI-Key": 'your-api-key',
+  //         "X-RapidAPI-Host": 'facts-by-api-ninjas.p.rapidapi.com',
+  //       },
+  //     })
+  //       .then((response) => {
+  //         response.json().then((data) => {
+  //           this.fact = data[0].fact;
+  //         });
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //       });
+  //   },
+  // },
+// };
+
+
+
+
+
+// export default {
+//   name: 'App',
+//   methods: {
+    async updateObservation() {
 
       const params = new URLSearchParams(window.location.search)
       const observationId = params.get('state')
       const code = params.get('code')
-      const message = ref('')
+      // const message = ref('')
 
       try {
         const url = new URL('/api/update', window.location.href)
@@ -44,7 +78,8 @@ export default {
         }
         const text = await response.text()
         console.log(text)
-        message.value = "Success! iNaturalist observation has been updated. CAMS will be updated within an hour."
+        
+        this.message = "Success! iNaturalist observation has been updated. CAMS will be updated within an hour."
         // const data = await response.json()
         // console.log(data)
         // Handle the API response data here
@@ -65,6 +100,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  padding: 12px 32px;
+  font-size: 16px;
+  border-radius: 8px;
 }
 </style>
 
