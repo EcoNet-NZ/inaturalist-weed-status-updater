4385c70a6324cab118bc19157926b96b402fb015
Nigel Charman <nigel.charman.nz@gmail.com>
2024-03-26 21:55:19 +1300

    Revert "Added reader. Base64 encode state"
    This reverts commit dd8de9df73eb742eb996d523774f63a12077ca55.

--- src/components/ObservationUpdater.vue
+++ src/components/ObservationUpdater.vue
@@ -63,7 +63,7 @@ export default {
       try {
         const url = new URL('/api/update', window.location.href)
         url.searchParams.set('auth-code', this.code)
-        url.searchParams.set('state', btoa(this.observationId))
+        url.searchParams.set('state', this.observationId)
         console.log(url)
         const response = await fetch(url, {
             method: "POST"
