613a4a0b3e6b874414d212e953199a47660d8c77
Nigel Charman <nigel.charman.nz@gmail.com>
2025-01-08 22:33:21 +1300

    Updated to use 'Date for next visit'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!--
 ====================================================================
-Copyright 2024 EcoNet.NZ
+Copyright 2024-2025 EcoNet.NZ
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
