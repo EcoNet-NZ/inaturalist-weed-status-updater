50ce7375efd3f8c38ee7fbbebb2d0e5474bec8fe
Nigel Charman <nigel.charman.nz@gmail.com>
2025-03-16 12:59:42 +1300

    Set deprecated date of next visit to undef

--- api/function_app.py
+++ api/function_app.py
@@ -50,7 +50,7 @@ def update_observation_fields(json, access_token):
 
     for key, value in json.items():
         if key != 'observationId':
-            if key == 18871 and value is None:  # Reset date for next visit
+            if value is None:
                 value = ''
 
             logging.info(f'Updating {key} to {value}')
